4b81a657b96328874f3b3908c09907baddc94fd9
sarirahmamaulida <sarirahmamaulida@gmail.com>
2020-01-16 09:04:42 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/sari/sqlitedemos01/dao/FriendDao.java
@@ -0,0 +1,14 @@
+package id.ac.poliban.sari.sqlitedemos01.dao;
+
+import java.util.List;
+
+import id.ac.poliban.sari.sqlitedemos01.domain.Friend;
+
+public interface FriendDao {
+    void insert(Friend f);
+    void update(Friend f);
+    void delete(int id);
+    Friend getAFriendById(int id);
+    List<Friend> getAllFriends();
+
+}
